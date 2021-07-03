894f87a8fefd00ab4ae849bfebe4d2b9429d8c65
Viv Zara <vzara88@hotmail.com>
2021-07-03 14:30:19 +1000

    Update to include JAWSDB requirements

--- config/connection.js
+++ config/connection.js
@@ -14,16 +14,19 @@ if (process.env.DATABASE_URL) {
   };
   sequelize = new Sequelize(process.env.DATABASE_URL, config);
 } else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-      host: "localhost",
-      dialect: "mysql",
-      port: 3306,
-    }
-  );
+  //condition ? do this : else do that
+  sequelize = process.env.JAWSDB_URL
+    ? new Sequelize(process.env.JAWSDB_URL)
+    : new Sequelize(
+        process.env.DB_NAME,
+        process.env.DB_USER,
+        process.env.DB_PASSWORD,
+        {
+          host: "localhost",
+          dialect: "mysql",
+          port: 3306,
+        }
+      );
 }
 
 module.exports = sequelize;
